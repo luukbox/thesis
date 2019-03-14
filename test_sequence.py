e95803d5f1bdf640eb9d74d399e56b999dc81b66
luukbox <lukassebastianmueller@gmail.com>
2019-03-14 03:14:49 +0100

    requirements updated, test_sequence now callable from cmd

--- test_sequence.py
+++ test_sequence.py
@@ -1,4 +1,10 @@
+import sys
 from sp800_22_tests import test_sequence, print_summary
 
-results = test_sequence("./binary_sequences/r_1600000.bin")
-print_summary(results)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        raise Exception(
+            "please provide the path of the sequences you want to test.")
+
+    results = test_sequence(sys.argv[1])
+    print_summary(results)
